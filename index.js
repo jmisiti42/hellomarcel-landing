68b804a0201f443eeed47c0eb0ba11f3cfacd2b2
jmisiti <jmisiti42@gmail.com>
2017-12-26 16:46:01 +0100

    ajout favicon

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const favicon = require('serve-favicon');
+const path = require('path');
 app.use(express.static('public'));
 app.use(favicon(path.join(__dirname,'public','images','favicon.ico')));
 app.listen(3000, () => console.log('Server running on port 3000'));

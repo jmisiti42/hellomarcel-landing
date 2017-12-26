ac87ca7dfec15634140cf44382f564b9ae1b6f3a
jmisiti <jmisiti42@gmail.com>
2017-12-26 19:10:25 +0100

    ajout plugin customer chat

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express');
 const app = express();
-const path = require('path');
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 app.listen(3000, () => console.log('Server running on port 3000'));

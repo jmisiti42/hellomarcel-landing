853fd14fe0e469bc76de2576b0149e6a4f30ddaa
jmisiti <jmisiti42@gmail.com>
2018-02-27 18:12:36 +0100

    Ajout des bitlys dans informations

--- models/url.js
+++ models/url.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 var UrlSchema = new mongoose.Schema({
 	url: {type: String, required: true},
+	created_at: {type: Date, default: Date.now() },
 	name: {type: String, required: true, unique: true}
 });
 
